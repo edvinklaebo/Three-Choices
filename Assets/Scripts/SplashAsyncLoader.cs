1ba63759c0da161f9feebc4578a84b81ba3652d4
Edvin <edvin.klaebo@gmail.com>
2026-01-29 23:09:55 +0100

    Add splash screen

---
+++ Assets/Scripts/SplashAsyncLoader.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SplashAsyncLoader : MonoBehaviour
+{
+    [Header("Scene Loading")]
+    [SerializeField] private string nextScene = "MainMenu";
+    [SerializeField] private Slider progressBar;
+    [SerializeField] private float minDisplayTime = 1.5f;
+
+    [Header("Logo Fade")]
+    [SerializeField] private Image logo;
+    [SerializeField] private float fadeInDuration = 0.5f;
+    [SerializeField] private float fadeOutDuration = 0.4f;
+
+    private IEnumerator Start()
+    {
+        // Fade logo in
+        yield return Fade(logo, 0f, 1f, fadeInDuration);
+
+        float startTime = Time.time;
+
+        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false;
+
+        while (op.progress < 0.9f)
+        {
+            progressBar.value = Mathf.Clamp01(op.progress / 0.9f);
+            yield return null;
+        }
+
+        progressBar.value = 1f;
+
+        float elapsed = Time.time - startTime;
+        if (elapsed < minDisplayTime)
+            yield return new WaitForSeconds(minDisplayTime - elapsed);
+
+        // Fade logo out
+        yield return Fade(logo, 1f, 0f, fadeOutDuration);
+
+        op.allowSceneActivation = true;
+    }
+
+    private IEnumerator Fade(Image img, float from, float to, float duration)
+    {
+        float t = 0f;
+        Color c = img.color;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            c.a = Mathf.Lerp(from, to, t / duration);
+            img.color = c;
+            yield return null;
+        }
+
+        c.a = to;
+        img.color = c;
+    }
+}
