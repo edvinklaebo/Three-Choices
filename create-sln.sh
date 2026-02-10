7b68bea77221dcad1e6c983cd0f336fba4fe9e74
Edvin <edvin.klaebo@gmail.com>
2026-02-10 17:55:45 +0100

    remove anything qodana related fuck that trash

--- create-sln.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-UNITY_EXE="/d/Unity/6000.3.6f1/Editor/Unity.exe"
-UNITY_DIR="/c/Users/Edvin/Roguelike PvE auto-battler"
-
-"$UNITY_EXE" \
-  -batchmode -quit \
-  -projectPath "$UNITY_DIR" \
-  -executeMethod Packages.Rider.Editor.RiderScriptEditor.SyncSolution
